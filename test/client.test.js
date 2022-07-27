c3e40fb2e7f41d467408d61cc175c29c1b60d9f4
FNahuelO <fosorio@aidforaids.org>
2022-07-27 12:13:59 -0300

    code-challenge

--- test/client.test.js
+++ test/client.test.js
@@ -2,14 +2,14 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import store from '../src/redux/store/index';
 import { Provider } from 'react-redux';
+
+
 import App from '../src/App.js';
-import axios from 'axios';
-
-
 
 const Component = () => (
     <Provider store={store}>
@@ -17,16 +17,9 @@ const Component = () => (
     </Provider>
 )
 
-jest.mock('axios')
-
-afterEach(() => {
-    jest.clearAllMocks();
-  });
-
 beforeEach(() => {
     render(<Component />)
 })
-
 
 test('should render component', () => {
     const title = screen.getByText(/reverse text/i);
@@ -34,7 +27,6 @@ test('should render component', () => {
 });
 
 test('should response text reverse', async() => {
-    axios.get.mockResolvedValue({ data: { text: 'alliterrac', success: true, palindrome: false}})
     const input = screen.getByLabelText(/insert text/i)
     const button = screen.getByRole("button", {name: /send/i})
     fireEvent.change(input, { target: { value: 'Carretilla'}})
@@ -43,32 +35,24 @@ test('should response text reverse', async() => {
     expect(cardText).toBeInTheDocument()
     expect(cardText).toHaveTextContent(/alliterrac/i)
 })
-/* 
-describe('should render svg', () => {
-    test('should render svg cancel', async() => {
-        axios.get.mockReturnValueOnce({
-                text:'luzazul',
-                succes: true,
-                palindrome: false      
-        })
-        const input = screen.getByLabelText(/insert text/i)
-        const button = screen.getByRole("button", {name: /send/i})
-        fireEvent.change(input, { target: { value: 'Luz azul'}})
-        fireEvent.click(button)
-        const palindrome = await screen.findByTestId("cancel")
-        expect(palindrome).toBeInTheDocument()
-    });
-    
-    test('should render svg check', async() => {
-        const input = screen.getByLabelText(/insert text/i)
-        const button = screen.getByRole("button", {name: /send/i})
-        fireEvent.change(input, { target: { value: 'Luz azul'}})
-        fireEvent.click(button)
-        const palindrome = await screen.findByTestId("check")
-        expect(palindrome).toBeInTheDocument()
-    }); 
-});*/
 
+test('should render svg cancel', async() => {
+    const input = screen.getByLabelText(/insert text/i)
+    const button = screen.getByRole("button", {name: /send/i})
+    fireEvent.change(input, { target: { value: 'Carretilla'}})
+    fireEvent.click(button)
+    const palindrome = await screen.findByTestId("cancel")
+    expect(palindrome).toBeInTheDocument()
+});
+
+test('should render svg check', async() => {
+    const input = screen.getByLabelText(/insert text/i)
+    const button = screen.getByRole("button", {name: /send/i})
+    fireEvent.change(input, { target: { value: 'Luz azul'}})
+    fireEvent.click(button)
+    const palindrome = await screen.findByTestId("check")
+    expect(palindrome).toBeInTheDocument()
+});
 
 
 
