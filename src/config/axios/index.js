aca0ee89b7ed5ec937b6e30be0b15325d1bc5a05
FNahuelO <fosorio@aidforaids.org>
2022-07-27 12:40:26 -0300

    code-challenge

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const baseUrl = axios.create({
-    baseURL: 'https://codewrite-api.herokuapp.com/' || process.env.REACT_APP_SERVER
+    baseURL: 'https://copywrite-api.herokuapp.com/' || process.env.REACT_APP_SERVER
   });
